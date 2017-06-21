f00a3697278c9da6a98f40e7db3e5e761f4cf51e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-06-21 22:27:38 +0200

    Initial code  commit

---
+++ tests/MessageKeywordFilterLoggerTest.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Tests\PSR3\KeywordFilter;
+
+use Prophecy\Argument;
+use Psr\Log\LoggerInterface;
+use Psr\Log\Test\LoggerInterfaceTest;
+use function WyriHaximus\PSR3\checkCorrectLogLevel;
+use WyriHaximus\PSR3\KeywordFilter\MessageKeywordFilterLogger;
+use function WyriHaximus\PSR3\processPlaceHolders;
+
+final class MessageKeywordFilterLoggerTest extends LoggerInterfaceTest
+{
+    /**
+     * @var array
+     */
+    public $logs = [];
+
+    public function getLogger()
+    {
+        $that = $this;
+        $logger = $this->prophesize(LoggerInterface::class);
+        $logger->log(
+            Argument::any(),
+            Argument::any(),
+            Argument::any()
+        )->will(function ($args) use ($that) {
+            list($level, $message, $context) = $args;
+            $message = (string)$message;
+            checkCorrectLogLevel($level);
+            $message = processPlaceHolders($message, $context);
+            $that->logs[] = $level . ' ' . $message;
+            return true;
+        });
+
+        return new MessageKeywordFilterLogger([], $logger->reveal());
+    }
+
+    public function getLogs()
+    {
+        return $this->logs;
+    }
+
+    public function testFilter()
+    {
+        $keywords = ['foo', 'bar'];
+        $logger = $this->prophesize(LoggerInterface::class);
+        $logger->log('info', 'faa bor', [])->shouldBeCalled();
+
+        $filter = new MessageKeywordFilterLogger($keywords, $logger->reveal());
+        $filter->log('info', 'foo');
+        $filter->log('info', 'bar');
+        $filter->log('info', 'faa bor');
+    }
+}
